e90fc36154b4b09499512ba15d916a7be767d239
krishnaa-1 <krishnamalviya272@gmail.com>
2025-02-18 04:47:17 +0530

    mongodb Driver updated

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ console.log(process.env.MONGO_URI);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection failed:", err));
 
